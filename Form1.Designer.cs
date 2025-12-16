cd45000c8dc784116b1f2134506c0cc242417b91
Ιωάννης Μπαλκούρας <giannosbalk@gmail.com>
2025-12-16 22:08:53 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace _2hAtomikh2
+{
+    partial class Atomikh2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Atomikh2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // Atomikh2
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Atomikh2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2hAtomikh2
+{
+    public partial class Atomikh2 : Form
+    {
+        int rows;           //Γραμμές
+        int col;            //Στήλες
+        int totalforms;     //Συνολικές Φόρμες που θα δημιουργηθούν
+        int formWidth;      //Οι διαστάσεις της κάθε φόρμας (Πλάτος)
+        int formHeight;     // (Ύψος)
+        public Atomikh2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)        //ΌΤΑΝ ΠΑΤΗΘΕΊ ΚΟΥΜΠΊ
+        {
+            timer1.Enabled = true;                //Ενεργοποιεί το Timer
+            rows = int.Parse(textBox1.Text);      //Δίνει τις τιμές του textbox στις σειρές
+            col = int.Parse(textBox2.Text);       //στις στήλες
+            totalforms = rows * col;              //Υπολογίζει τις συνολικές φόρμες      
+            this.Location = new Point(0, 0);      //Τοποθετεί την φόρμα πάνω αριστερά ώστε να μπορεί ο χρήστης να ξανατρέξει το πρόγραμμα
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)          //ΌΤΑΝ ΞΕΚΙΝΉΣΕΙ ΤΟ TIMER  
+        {
+            Atomikh2 f2 = new Atomikh2();                              //Δημιουργεί Φόρμα
+            formWidth = Screen.PrimaryScreen.Bounds.Width / col;       //Δίνει τις διαστάσεις τις οθόνης
+            formHeight = Screen.PrimaryScreen.Bounds.Height / rows;
+            this.Size = new Size(600,400);                             //Μέγεθος κύριας φόρμας
+            f2.Size = new Size(formWidth, formHeight);                 //Οι διαστάσεις
+            int currentCol = (totalforms-1) % col;                     //Τρέχον στηλη
+            int currentRow = (totalforms-1) / col;                      //Τρέχον σειρά
+            f2.Location = new Point(currentCol * formWidth, currentRow * formHeight);    //Τοποθεσία φόρμας
+            f2.StartPosition = FormStartPosition.Manual;               //Αρχική θέση φόρμας όταν ξεκινήσει το Timer
+            Random random = new Random();                              //Τυχαία χρώματα
+            Color randomColor = Color.FromArgb(random.Next(200),random.Next(200),random.Next(200));  //Διαστάσεις ανάμεσα στα χρώματα
+            f2.BackColor = randomColor;                                //Αλλάζει το χρώμα κάθε φόρμας
+            f2.Controls.Clear();                                       //Αδειάζει την φόρμα απτα controls
+            Label label = new Label();                                 //Προσθέτει Label
+            label.Text = "Good Evening Teacher -p21228\n" + (currentRow+1) + "," + (currentCol+1);  //Το Text του Label *Για κάποιο λόγο μου ξεκίναγε απο το μηδέν
+            label.TextAlign = ContentAlignment.MiddleCenter;           //Θέση Text Label
+            label.Dock = DockStyle.Fill;                               //Να καλύψει όλη την φόρμα ώστε να μπεί στο κέντρο
+            f2.Controls.Add(label);                                    //Προσθέτουμε το Label στην φόρμα 
+            f2.Show();                                                 //Εμφανίζει τις φόρμες
+            totalforms--;                                              //Μειώνει τις φόρμες ώστε να μην δημιουργήσει παραπάνω
+            if (totalforms <= 0)                        //Έλεγχος ώστε να τερματίσει το Timer όταν ολοκληρωθούν όλες οι φόρμες
+            {
+              timer1.Enabled = false;     
+            }
+                                                       //ΤΕΛΟΣ
+        }
+
+        
+    }
+}
